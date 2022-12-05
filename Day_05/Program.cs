bed6b3850ab5799e6b16668e85482ef2e61bcd2f
Jan Eric Babula <jan.eric.babula@gmail.com>
2022-12-05 10:46:17 +0100

    Day 5 - Part 1

--- Day_05/Program.cs
+++ Day_05/Program.cs
@@ -13,6 +13,59 @@ namespace Day_05
         {
             string[] input = File.ReadAllLines("input.txt");
 
+            int stack_amount = (int)((input[0].Length + 1) / 4);
+            List<List<char>> stacks_tmp = new List<List<char>>();
+
+            for (int i = 0; i < stack_amount; i++)
+            {
+                stacks_tmp.Add(new List<char>());
+            }
+
+            int move_start = 0;
+            while(move_start < input.Length && input[move_start][1] != '1')
+            {
+                for(int i = 0; i < stack_amount; i++)
+                {
+                    if(input[move_start][i*4 + 1] != ' ')
+                    {
+                        stacks_tmp[i].Add(input[move_start][i * 4 + 1]);
+                    }
+                }
+                move_start++;
+            }
+            move_start += 2;
+
+            List<Stack<char>> stacks = new List<Stack<char>>();
+            foreach(List<char> l  in stacks_tmp)
+            {
+                l.Reverse();
+                stacks.Add(new Stack<char>(l));
+            }
+
+            int amount, origin, target;
+            for (int i = move_start; i < input.Length; i++)
+            {
+                string[] move = input[i].Split(new char[] { ' ' });
+                amount = int.Parse(move[1]);
+                origin = int.Parse(move[3]) - 1;
+                target = int.Parse(move[5]) - 1;
+
+                for(int a = 0; a < amount; a++)
+                {
+                    if(stacks[origin].Count > 0)
+                    {
+                        stacks[target].Push(stacks[origin].Pop());
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach(Stack<char> sc in stacks)
+            {
+                sb.Append(sc.Peek());
+            }
+            Console.WriteLine($"Top crates: {sb}");
+            
             Console.ReadLine();
         }
     }
