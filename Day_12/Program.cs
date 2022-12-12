dcf5b69e0480cc0e4e5c09bdfff29fe749263870
Jan Eric Babula <jan.eric.babula@gmail.com>
2022-12-12 02:14:09 +0100

    Day 12 - Init

---
+++ Day_12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            Console.ReadLine();
+        }
+    }
+}
